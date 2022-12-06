fd4635d1d74edcb3f70bcf82f2a1e2fd8fee4b7b
Einar Strandberg <einargs@gmail.com>
2022-12-05 20:59:53 -0800

    Cleaned day 5 more

--- src/Day5.hs
+++ src/Day5.hs
@@ -11,7 +11,6 @@ import Data.Sequence qualified as S
 import Data.Functor (($>))
 import Data.List (foldl')
 import Data.Foldable qualified as F
-import Debug.Trace (trace, traceShowM)
 
 import Day
 
@@ -20,64 +19,44 @@ type Stack = [Char]
 type Dock = Seq Stack
 
 parseDock :: Parser Dock
-parseDock = fmap reverse <$> (firstRow >>= readRow) where
+parseDock = fmap reverse <$> readRow firstRowF where
   box = P.between (C.string "[") (C.string "]") C.upperChar
   empty = C.string "   "
   mbBox = P.try (Just <$> box) <|> P.try (empty $> Nothing)
   row :: Parser [Maybe Char]
-  row = mbBox `P.sepBy` C.string " "
+  row = mbBox `P.sepBy` C.string " " <* C.newline
   combine S.Empty [] = S.Empty
   combine (s :<| d) (Just v:ls) = (v:s) :<| combine d ls
   combine (s :<| d) (Nothing:ls) = s :<| combine d ls
-  firstRow :: Parser Dock
-  firstRow = do
+  firstRowF boxes = S.fromList $ replicate (length boxes) []
+  readRow :: ([Maybe Char] -> Dock) -> Parser Dock
+  readRow f = do
     newBoxes <- row
-    C.newline
-    let emptyDock = S.fromList $ replicate (length newBoxes) []
-    pure $ combine emptyDock newBoxes
-  readRow :: Dock -> Parser Dock
-  readRow d = do
-    newBoxes <- row
-    C.newline
-    let d' = combine d newBoxes
-        end = (C.string " " *> C.digitChar) $> d'
-    P.try end <|> readRow d'
+    let d' = combine (f newBoxes) newBoxes
+    P.try (readRow $ const d') <|> pure d'
 
 data Instr = Instr {move::Int, from::Int, to::Int}
 
 parseInstr :: Parser Instr
-parseInstr = do
-  C.string "move "
-  move <- L.decimal
-  C.string " from "
-  from <- L.decimal
-  C.string " to "
-  to <- L.decimal
-  C.newline
-  pure $ Instr move (from-1) (to-1)
+parseInstr = Instr <$> n "move " <*> sub (n " from ")
+    <*> sub (n " to ") where
+  n s = C.string s *> L.decimal
+  sub p = subtract 1 <$> p
 
 parseInput :: Parser (Dock, [Instr])
 parseInput = do
   d <- parseDock
-  skipLine
-  skipLine
-  is <- P.some $ P.try parseInstr
+  skipLine <* skipLine
+  is <- parseInstr `P.sepEndBy` C.newline
   pure (d, is)
-  where
-    skipLine = P.skipManyTill C.printChar C.newline
+  where skipLine = P.skipManyTill C.printChar C.newline
 
-solve :: ([a] -> [a]) -> Parser String
-solve h = do
-  (d, is) <- parseInput
-  let finalDock = foldl' f d is
-  pure $ top finalDock
-  where
-    f dock Instr{move,from,to} = S.update to e' $ S.update from s' dock where
-      s = dock `S.index` from
-      s' = drop move s
-      e = dock `S.index` to
-      e' = h (take move s) <> e
-    top = F.toList . fmap head
+solve :: (forall a. [a] -> [a]) -> Parser String
+solve h = top . uncurry (foldl' f) <$> parseInput where
+  f dock Instr{move,from,to} = S.adjust toF to $ S.adjust fromF from dock
+    where fromF = drop move
+          toF e = h (take move (dock `S.index` from)) <> e
+  top = F.toList . fmap head
 
 part1 :: Parser String
 part1 = solve reverse
@@ -89,4 +68,4 @@ wrap :: Parser String -> Text -> Text
 wrap p = T.pack . runParse p
 
 day5 :: Day
-day5 = mkDay "5" (wrap part1) (wrap part2)
+day5 = answeredDay "5" (wrap part1) (wrap part2) "RNZLFZSJH" "CNSFCGJSM"
