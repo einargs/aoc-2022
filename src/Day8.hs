630479a4a2e096fdbba95e836317b5a949ef0123
Einar Strandberg <einargs@gmail.com>
2022-12-07 22:57:14 -0800

    Correct day 8 part 1

--- src/Day8.hs
+++ src/Day8.hs
@@ -9,32 +9,51 @@ import Data.List qualified as L
 import Data.Functor (($>))
 import Control.Monad (void, (<=<))
 import Debug.Trace qualified as DT
+import Data.Vector qualified as V
+import Data.Vector.Unboxed qualified as UV
+import Control.Arrow (first, second)
 
 import Day
 
-type Forest = [[Int]]
+type Forest = V.Vector (UV.Vector Int)
+type Point = (Int, Int)
+
+infixl 9 #
+
+(#) :: Forest -> Point -> Int
+v # (ui, bi) = (v V.! bi) UV.! ui
+
+size :: Forest -> Point
+size f = (UV.length $ f V.! 0, V.length f)
 
 parseForest :: Text -> Forest
-parseForest = fmap (fmap f . T.unpack) . T.splitOn "\n" . T.strip where
+parseForest = V.fromList . fmap buildRow . T.splitOn "\n" . T.strip where
   f c = fromEnum c - fromEnum '0'
+  buildRow = UV.fromList . fmap f . T.unpack
 
-sightDepth [] = error "should not happen"
-sightDepth (t:ts) = f (1,[0]) t ts where
-  f (i, acc) _ [] = acc
-  f (i, acc) m (c:xs) | m < c = f (i+1, i:acc) c xs
-                      | otherwise = f (i+1, acc) m xs
+indices :: Forest -> [Point]
+indices f = [0..x-1] >>= g where
+  (x, y) = size f
+  g i = (i,) <$> [0..y-1]
 
-rowIndices :: [Int] -> [Int]
-rowIndices row = sightDepth row <> fmap (l-) (sightDepth row) where
-  l = length row - 1
-allRows :: Forest -> [(Int, Int)]
-allRows = snd <=< L.scanl' f (0, []) where
-  f (i, _) row = (i+1, DT.traceShowId $ (,i) <$> rowIndices row)
+sightline :: Forest -> Point -> (Point -> Point) -> [Int]
+sightline f s inc = g [] $ inc s where
+  (mx, my) = size f
+  bounded (x,y) = x < mx && 0 <= x && y < my && 0 <= y
+  g acc p | bounded p = g (f # p:acc) (inc p)
+          | otherwise = acc
+
+visible :: Forest -> (Int, Int) -> Bool
+visible f p = check up || check down || check left || check right where
+  height = f # p
+  check g = all (<height) $ sightline f p g
+  up = second (subtract 1)
+  down = second (+1)
+  left = first (subtract 1)
+  right = first (+1)
+
 part1 :: Forest -> Int
-part1 forest = length $ DT.traceShowId $ L.nub $ visibleTrees where
-  visibleTrees = allRows forest <> top where
-    top = f <$> allRows (L.transpose forest)
-    f (a, b) = (b, a)
+part1 f = length $ filter (visible f) $ indices f
 
 part2 :: Forest -> Int
 part2 = undefined
