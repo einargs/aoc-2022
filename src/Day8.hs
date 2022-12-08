2c942582acee6d26e1b5d22db21a1cb39d8e02ef
Einar Strandberg <einargs@gmail.com>
2022-12-07 23:14:35 -0800

    Final day 8

--- src/Day8.hs
+++ src/Day8.hs
@@ -2,13 +2,6 @@ module Day8 (day8) where
 
 import Data.Text (Text)
 import Data.Text qualified as T
-import Text.Megaparsec ((<|>))
-import Text.Megaparsec qualified as P
-import Text.Megaparsec.Char qualified as C
-import Data.List qualified as L
-import Data.Functor (($>))
-import Control.Monad (void, (<=<))
-import Debug.Trace qualified as DT
 import Data.Vector qualified as V
 import Data.Vector.Unboxed qualified as UV
 import Control.Arrow (first, second)
@@ -37,7 +30,7 @@ indices f = [0..x-1] >>= g where
   g i = (i,) <$> [0..y-1]
 
 sightline :: Forest -> Point -> (Point -> Point) -> [Int]
-sightline f s inc = g [] $ inc s where
+sightline f s inc = reverse $ g [] $ inc s where
   (mx, my) = size f
   bounded (x,y) = x < mx && 0 <= x && y < my && 0 <= y
   g acc p | bounded p = g (f # p:acc) (inc p)
@@ -55,11 +48,23 @@ visible f p = check up || check down || check left || check right where
 part1 :: Forest -> Int
 part1 f = length $ filter (visible f) $ indices f
 
+scenicScore :: Forest -> Point -> Int
+scenicScore f p = score up * score down * score left * score right where
+  height = f # p
+  metric i [] = i
+  metric i (x:xs) | x<height = metric (i+1) xs
+                  | otherwise = i+1
+  score g = metric 0 $ sightline f p g
+  up = second (subtract 1)
+  down = second (+1)
+  left = first (subtract 1)
+  right = first (+1)
+
 part2 :: Forest -> Int
-part2 = undefined
+part2 f = maximum $ scenicScore f <$> indices f
 
 wrap :: (Forest -> Int) -> Text -> Text
 wrap f = T.pack . show . f . parseForest
 
 day8 :: Day
-day8 = mkDay "8" (wrap part1) (wrap part2)
+day8 = answeredDay "8" (wrap part1) (wrap part2) "1779" "172224"
