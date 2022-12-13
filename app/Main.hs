e14d26ca2796c930f842ce0ab522b84a35ced607
Einar Strandberg <einargs@gmail.com>
2022-12-12 16:10:47 -0800

    Day 12 with search package

--- app/Main.hs
+++ app/Main.hs
@@ -25,12 +25,13 @@ import Day8
 import Day9
 import Day10
 import Day11
+import Day12
 
 days :: Map Text Day
 days = M.fromList $ f <$> dayList where
   f day@Day{dayName} = (dayName, day)
   dayList = [day1, day2, day2Golf, day3, day4, day4Golf, day5, day6, day6Alt,
-    day7, day7Zipper, day8, day9, day10, day11]
+    day7, day7Zipper, day8, day9, day10, day11, day12]
 
 runPart :: Bool -> Text -> Maybe Text -> IO ()
 runPart measure ans mbAns = do
--- src/Day12.hs
+++ src/Day12.hs
@@ -2,26 +2,75 @@ module Day12 (day12) where
 
 import Data.Text (Text)
 import Data.Text qualified as T
-import Text.Megaparsec ((<|>))
-import Text.Megaparsec qualified as P
-import Text.Megaparsec.Char qualified as C
-import Text.Megaparsec.Char.Lexer qualified as L
+import Data.Text.Encoding (encodeUtf8)
 import Data.Functor (($>))
 import Control.Monad (void)
-import Data.Sequence (Seq((:<|), (:|>)))
-import Data.Sequence qualified as S
+import Data.ByteString qualified as BS
 import Data.Foldable (foldl', toList)
+import Algorithm.Search qualified as Alg
+import Data.Maybe (fromJust, mapMaybe, listToMaybe)
+import Data.Set (Set)
+import Data.Set qualified as S
+import Debug.Trace qualified as DT
 
 import Day
 
-part1 :: Parser Int
-part1 = undefined
+type Pos = (Int, Int)
 
-part2 :: Parser Int
-part2 = undefined
+label :: Show a => String -> a -> a
+label l v = DT.trace (l <> " " <> show v) v
 
-wrap :: Show a => Parser a -> Text -> Text
-wrap p = T.pack . show . runParse p
+-- length data start end
+data Region = Region Int BS.ByteString Pos Pos
+
+instance Show Region where
+  show r@(Region l bs start end) = unlines ps where
+    f p | p == start = 'S'
+        | p == end = 'E'
+        | otherwise = toEnum $ r#p + fromEnum 'a'
+    h = regionHeight r
+    mkRow y = f . (,y) <$> [0..l-1]
+    ps = mkRow <$> [0..(h-1)]
+
+regionHeight :: Region -> Int
+regionHeight (Region w bs _ _) = BS.length bs `div` w
+
+toRegion :: Text -> Region
+toRegion t = Region w raw start end where
+  (Just w) = T.findIndex (=='\n') t
+  (Just si) = BS.elemIndex (toEnum $ fromEnum 'S') raw
+  (Just ei) = BS.elemIndex (toEnum $ fromEnum 'E') raw
+  start = (si `mod` w, si `div` w)
+  end = (ei `mod` w, ei `div` w)
+  raw = encodeUtf8 $ T.filter (/='\n') t
+
+infixl 9 #
+(#) :: Region -> Pos -> Int
+(Region l bs _ _) # (x, y) = f $ bs `BS.index` ((l*y) + x) where
+  f c | c == toEnum (fromEnum 'S') = 0
+      | c == toEnum (fromEnum 'E') = 25
+      | otherwise = fromEnum c - fromEnum 'a'
+
+pathDist :: Region -> Pos -> Maybe Int
+pathDist reg@(Region w _ _ end) start = length <$> answer where
+  answer = Alg.bfs neighbors (==end) start
+  neighbors p@(x,y) = filter f raw where
+    raw = [(x-1, y), (x, y+1), (x, y-1), (x+1, y)]
+    f p'@(x,y) = x >= 0 && y >= 0 && x < w && y < regionHeight reg
+      && (reg # p') <= (reg # p) + 1 && p' /= p
+
+part1 :: Region -> Int
+part1 r@(Region _ _ start _) = fromJust $ pathDist r start
+
+part2 :: Region -> Int
+part2 r@(Region l _ _ _) = minimum $ mapMaybe (pathDist r) ps where
+  f p = r#p == 0
+  h = regionHeight r
+  mkRow y = (,y) <$> [0..l-1]
+  ps = filter f $ [0..(h-1)] >>= mkRow
+
+wrap :: (Region -> Int) -> Text -> Text
+wrap f = T.pack . show . f . toRegion
 
 day12 :: Day
 day12 = mkDay "12" (wrap part1) (wrap part2)
